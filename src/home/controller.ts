ecad406e4a73850921195910006e73df9c52abe5
Evert Pot <me@evertpot.com>
2020-01-22 21:15:20 -0500

    HTML version of home

--- src/home/controller.ts
+++ src/home/controller.ts
@@ -1,10 +1,23 @@
-import Controller from '@curveball/controller';
+import { Controller, accept, method } from '@curveball/controller';
 import { Context } from '@curveball/core';
 import hal from './formats/hal';
+import markdown from './formats/markdown';
 
 class HomeController extends Controller {
 
-  get(ctx: Context) {
+  @accept('text/markdown')
+  @method('GET')
+  getMarkdown(ctx: Context) {
+
+    const version = require('../../package.json').version;
+    ctx.response.body = markdown(version, ctx.state.user);
+    ctx.response.headers.set('Content-Type', 'text/markdown');
+
+  }
+
+  @accept('application/json')
+  @method('GET')
+  getJson(ctx: Context) {
 
     const version = require('../../package.json').version;
     ctx.response.body = hal(version, ctx.state.user);
---
+++ src/home/formats/markdown.ts
@@ -0,0 +1,86 @@
+import { User } from '../../user/types';
+import hal from './hal';
+
+export default (version: string, authenticatedUser: User): string => {
+
+  const json = JSON.stringify(
+    hal(version, authenticatedUser),
+    null,
+    2
+  );
+
+  return `
+# A12n Server
+
+Version: ${version}
+
+## User
+
+* [Logout](/logout)
+
+
+## OAuth2 endpoints
+
+* [Introspect](/introspect)
+* [Token](/token)
+* [Authorize](/authorize)
+
+\`\`\`json
+${json}
+\`\`\`
+
+
+`;
+    /*
+
+  const result: HalResource = {
+    _links: {
+      'self': { href: '/', title: 'Auth API Home' },
+      'authenticated-as': { href: '/user/' + authenticatedUser.id, title: authenticatedUser.nickname },
+      'authorize' : { href: '/authorize', title: 'OAuth2 authorize endpoint', type: 'text/html' },
+      'change-password': { href: '/changepassword', title: 'Change user\'s password' },
+      'introspect' : {
+        href: '/introspect',
+        title: 'OAuth2 Token Introspection',
+        hints: {
+          allow: ['POST'],
+        }
+      },
+      'logout': {
+        href: '/logout',
+        title: 'Log out'
+      },
+      'token': {
+        href: '/token',
+        title: 'OAuth2 protocol endpoint'
+      },
+      'privilege-collection': {
+        href: '/privilege',
+        title: 'List of available privileges',
+      },
+      'user-collection': { href: '/user', title: 'List of users'},
+      'validate-bearer': {
+        href: '/validate-bearer',
+        title: 'Validate a OAuth2 bearer token',
+        hints: {
+          status: 'deprecated',
+        }
+      },
+
+      'validate-totp': { href: '/validate-totp', title: 'Validate a TOTP 2FA token + bearer token'},
+      'oauth_server_metadata_uri' : { href: '/.well-known/oauth-authorization-server', title: 'OAuth 2.0 Authorization Server Metadata' }
+    },
+    version: version,
+  };
+
+  if (getSetting('registration.enabled')) {
+    result._links.registration = {
+      href: '/register',
+      title: 'Create a new user account',
+      type: 'text/html'
+    };
+  }
+
+  return result;
+  */
+};
